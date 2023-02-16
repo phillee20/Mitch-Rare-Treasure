b2faf18eb44c8ffde575e4c7c6e26bc0e57d6145
phillee20 <phillee5647@hotmail.com>
2023-02-16 14:24:40 +0000

    Insert data

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,17 @@
 const db = require("./index");
-const format = require('pg-format')
+const format = require("pg-format");
 
 const seed = ({ shopData, treasureData }) => {
-	return db.query(`DROP TABLE IF EXISTS treasures;`).then(() => {
-
-		return db.query(`DROP TABLE IF EXISTS shops;`)
-		// drop any existing shops table
-	})
-	// then: create some new tables - but which first and why?
-	.then(() => {
-		return db.query(`
+  return (
+    db
+      .query(`DROP TABLE IF EXISTS treasures;`)
+      .then(() => {
+        return db.query(`DROP TABLE IF EXISTS shops;`);
+        // drop any existing shops table
+      })
+      // then: create some new tables - but which first and why?
+      .then(() => {
+        return db.query(`
 		CREATE TABLE shops (
 			shop_id SERIAL PRIMARY KEY,
 			shop_name VARCHAR(50) NOT NULL,
@@ -18,10 +20,9 @@ const seed = ({ shopData, treasureData }) => {
 		);
 		
 		`);
-
-	})
-		.then(() => {
-			return db.query(`
+      })
+      .then(() => {
+        return db.query(`
 		CREATE TABLE treasures (
         treasure_id SERIAL PRIMARY KEY,
         treasure_name VARCHAR(50) NOT NULL,
@@ -30,11 +31,25 @@ const seed = ({ shopData, treasureData }) => {
 		cost_at_auction FLOAT(10) NOT NULL,
 		shop_id INT REFERENCES shops(shop_id)
 		);
-			`)
+			`);
+      })
+      .then(() => {
+        const formattedShopsData = shopData.map((shop) => {
+          return Object.values(shop);
+        });
+        const insertShopData = format(
+          `
+		INSERT INTO shops (
+			shop_name, owner, slogan
+		)
+		VALUES %L RETURNING *;`,
+          formattedShopsData
+        );
+        return db.query(insertShopData);
+      })
+  );
 
-
-	})
-	// then: insert the raw data into the tables.
+  // then: insert the raw data into the tables.
 };
 
 module.exports = seed;
