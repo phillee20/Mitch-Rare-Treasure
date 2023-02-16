c444a9100a9eab0c6f23d449d1527bb73ea33494
Hanieh73 <hanieh.zaab1@gmail.com>
2023-02-16 13:43:28 +0000

    part1

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,39 @@
-const db = require("./");
+const db = require("./index");
+const format = require('pg-format')
 
 const seed = ({ shopData, treasureData }) => {
 	return db.query(`DROP TABLE IF EXISTS treasures;`).then(() => {
+
+		return db.query(`DROP TABLE IF EXISTS shops;`)
 		// drop any existing shops table
-	});
+	})
 	// then: create some new tables - but which first and why?
+	.then(() => {
+		return db.query(`
+		CREATE TABLE shops (
+			shop_id SERIAL PRIMARY KEY,
+			shop_name VARCHAR(50) NOT NULL,
+			owner VARCHAR(50) NOT NULL,
+			slogan TEXT 
+		);
+		
+		`);
+
+	})
+		.then(() => {
+			return db.query(`
+		CREATE TABLE treasures (
+        treasure_id SERIAL PRIMARY KEY,
+        treasure_name VARCHAR(50) NOT NULL,
+		colour VARCHAR(50) NOT NULL,
+		age INT NOT NULL,
+		cost_at_auction FLOAT(10) NOT NULL,
+		shop_id INT REFERENCES shops(shop_id)
+		);
+			`)
+
+
+	})
 	// then: insert the raw data into the tables.
 };
 
---
+++ listen.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+
+const PORT = 9090;
+
+app.listen(PORT, () => {
+console.log(`app is listening on ${PORT}`)
+
+})
